dd74fae52beac9ed6b0153598ccd976468ee5465
olof-sky <oloof92@gmail.com>
2022-07-19 18:01:54 +0200

    Add project api's

--- server/api/v1/routes/project.js
+++ server/api/v1/routes/project.js
@@ -1,6 +1,32 @@
 const express = require('express');
-const router = express.Router();
+const cors = require('cors');
 const project = require('../services/project');
+const router = express.Router();
+router.use(cors());
+router.use(express.json({
+  type: 'application/json',
+}))
+router.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+
+/******* 
+Create new project CURL cmd
+curl -H "Content-Type: application/json" -d '{"projectName":"xyz","projectType":"xyz"}' http://localhost:3002/api/v1/project/create 
+*******/
+
+/* CREATE project. { projectName, projectType } */
+router.post('/create', async function(req, res, next) {
+  try {
+    res.json(await project.create(req));
+  } catch (err) {
+    console.error(`Error while creating project `, err.message);
+    next(err);
+  }
+});
 
 /* GET projects. */
 router.get('/getMultiple', async function(req, res, next) {
@@ -23,16 +49,38 @@ router.get('/getById/:id', async function(req, res, next) {
   }
 });
 
-/* CREATE project. */
-router.post('/create', async function(req, res, next) {
-  let newProject = req.body
+/* UPDATE project name. { projectName } */
+router.put('/updateProjectName/:id', async function(req, res, next) {
+  let projectId = req.params.id;
   try {
-    res.send(await project.create(newProject));
-    res.json(await project.create(newProject));
+    res.json(await project.updateProjectName(req, projectId));
   } catch (err) {
-    console.error(`Error while creating project `, err.message);
+    console.error(`Error while updating project `, err.message);
     next(err);
   }
 });
+
+/* UPDATE project type. { projectType } */
+router.put('/updateProjectType/:id', async function(req, res, next) {
+  let projectId = req.params.id;
+  try {
+    res.json(await project.updateProjectType(req, projectId));
+  } catch (err) {
+    console.error(`Error while updating project `, err.message);
+    next(err);
+  }
+});
+
+/* DELETE project. */
+router.delete('/deleteProject/:id', async function(req, res, next) {
+  let projectId = req.params.id;
+  try {
+    res.json(await project.deleteProject(projectId));
+  } catch (err) {
+    console.error(`Error while deleting project `, err.message);
+    next(err);
+  }
+});
+
 
 module.exports = router;
--- server/api/v1/services/project.js
+++ server/api/v1/services/project.js
@@ -1,75 +1,90 @@
 const db = require('./db');
+const uuid = require("uuid");
 const helper = require('../../../helper');
 const config = require('../../../config');
 
-// Route to get multiple projects
+// Route for creating a project
+async function create(content){
+  const projectId = uuid.v4();
+  const projectName = await content.body.projectName;
+  const projectType = await content.body.projectType;
+  await db.query(`INSERT INTO project (project_id, project_name, project_type) VALUES ('${projectId}', '${projectName}','${projectType}')`);
+  const meta = {
+    projectName: projectName,
+    projectType: projectType};
+  
+  return {
+    meta
+  }
+}
+
+// Get multiple projects
 async function getMultiple(page = 1){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(`SELECT * FROM project LIMIT ${offset},${config.listPerPage}`);
   const data = helper.emptyOrRows(rows);
   const meta = {page};
+  console.log(data)
 
   return {
     data,
     meta
   }
 }
-    
-// Route to get one project
+
+// Get one project
 async function getById(projectId){
-  const rows = await  db.query(`SELECT * FROM project WHERE project_id = ${projectId}`);
-  const data = helper.emptyOrRows(rows);
+  const rows = await db.query(`SELECT * FROM project WHERE project_id = ${projectId}`);
+  const project = helper.emptyOrRows(rows);
   const meta = {projectId};
   
   return {
-    data,
+    project,
     meta
   }
 }
 
-
-// Route for creating a project
-async function create(newProject){
-  const rows = await db.query(`INSERT INTO project (project_id, project_name, type) VALUES (3, ${newProject.projectName}, ${newProject.type})`);
-  const data = helper.emptyOrRows(rows);
+// Update a project name
+async function updateProjectName(request, projectId){
+  const projectName = await request.body.projectName;
+  await db.query(`UPDATE project SET project_name = '${projectName}' WHERE project_id = '${projectId}'`);
   const meta = {
-    project_name: newProject.project_name, 
-    type: newProject.type};
+    projectName: projectName,
+  };
   
   return {
-    data,
     meta
   }
 }
-    
 
-
-
-
-// router.post('/api/v1/project/create', (req,res)=> {
+// Update a project type
+async function updateProjectType(request, projectId){
+  const projectType = await request.body.projectType;
+  await db.query(`UPDATE project SET project_type = '${projectType}' WHERE project_id = '${projectId}'`);
+  const meta = {
+    projectType: projectType,
+  };
+  return {
+    meta,
+  }
+}
     
-//     const projectName = req.body.projectName;
-//     const type = req.body.type;
-    
-//     db.query("INSERT INTO project (project_name, type) VALUES (?,?)",[projectName,type], (err,result)=>{
-//        if(err) {
-//        console.log(err)
-//        } 
-//        console.log(result)
-//     });   })
-    
-    // Route to delete a project
-    
-// router.delete('/api/v1/project/delete/:id',(req,res)=>{
-//     const id = req.params.id;
-    
-//     db.query("DELETE FROM project WHERE id= ?", id, (err,result)=>{
-//     if(err) {
-//     console.log(err)
-//             } }) })
+// Delete a project
+async function deleteProject(projectId){
+  await db.query(`DELETE FROM project WHERE project_id = '${projectId}'`);
+  const meta = {
+    projectId: projectId,
+  };
+  return {
+    meta,
+  }
+}
 
 module.exports = {
+  create,
   getMultiple,
   getById,
-  create,
+  updateProjectName,
+  updateProjectType,
+  deleteProject,
 }
