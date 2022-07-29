cbe50a24851af1c401274c5aea863d69d9a35f54
olof-sky <oloof92@gmail.com>
2022-07-29 14:48:34 +0200

    Add verification on refreshtoken

--- server/middleware/verifyRefreshToken.js
+++ server/middleware/verifyRefreshToken.js
@@ -12,7 +12,8 @@ module.exports = async function(req, res, next) {
   }
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_KEY, async (error, user) => {
     if (error) res.status(400).json('Invalid Refresh Token');
-    res.user = await db.User.scope('withHash').findByPk(user.user_id);
+    res.user = await db.User.scope('withAll').findByPk(user.user_id);
+    if (refreshToken !== res.user.refresh_token) { res.status(400).json('Outdated Refresh Token'); }
     next();
   })
 };
