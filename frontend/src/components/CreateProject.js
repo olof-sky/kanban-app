480e90b8d6727b9d86b58203b96940358ce14fd3
olof-sky <oloof92@gmail.com>
2022-08-05 19:26:04 +0200

    Add styling

--- frontend/src/components/CreateProject.js
+++ frontend/src/components/CreateProject.js
@@ -1,33 +1,32 @@
 import React,{useState} from 'react';
 import axios from 'axios'
 import { refreshToken } from '../helper'
+import { IoMdCloseCircle } from 'react-icons/io';
 import '../styles/components/CreateProject.scss'
 
-function CreateProject() {
+function CreateProject(props) {
   const [projectName,setProjectName] = useState("");
-  const [projectType,setProjectType] = useState("");
+  const [projectType,setProjectType] = useState("Kanban");
   const [projectAdmin,setProjectAdmin] = useState(JSON.parse(sessionStorage.getItem('User'))['user_id']);
+  const {toggleShowCreateWindow} = props;
 
   const submitProject = () => {
     try {
+      console.log(projectType)
       axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/user/getById/${projectAdmin}`).then((res)=>{
         const projectAdmins = {
           "admins":[
             //SET "admin" by user_id
             {"user_id": `${projectAdmin}`, 
-             "user_name": `${res.data.first_name} ${res.data.last_name}`}
-          ]
+             "user_name": `${res.data.first_name} ${res.data.last_name}`}]
         }
-        axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/v1/project/create`,
-          {
+        axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/v1/project/create`,{
             project_name: projectName,
             project_type: projectType,
-            project_admins: projectAdmins,
-          },
-          { headers: { Authorization:sessionStorage.getItem('Token') } }
+            project_admins: projectAdmins,}, { headers: { Authorization:sessionStorage.getItem('Token') }}
         )
         refreshToken();
-        window.location = "/projects";
+        window.location = "/projects"
       });
     }
     catch(err) {console.log(err)}
@@ -36,14 +35,19 @@ function CreateProject() {
   return (
     <div className="create-project-body">
       <div className="create-project-card">
-        <label>Project Name: </label>
-        <input type="text" onChange={(e)=> {
-          setProjectName(e.target.value)
-        }}/>
-        <label>Project Type: </label>
-        <input type="text" onChange={(e)=>{
-          setProjectType(e.target.value)}}/>
-        <button onClick={submitProject}>Submit Project</button>
+        <IoMdCloseCircle className="close-icon" onClick={toggleShowCreateWindow}></IoMdCloseCircle>
+        <div className="create-project-form">
+          <h3>Create a new project</h3>
+          <input autoFocus placeholder="Project name" type="text" onChange={(e)=> {
+            setProjectName(e.target.value)
+          }}/>
+          <select type="text" onChange={(e)=>{
+            setProjectType(e.target.value)}}>
+            <option value="Kanban">Kanban</option>
+            <option disabled value="">...</option>
+          </select>
+          <button className="btn" onClick={submitProject}>Create</button>
+        </div>
       </div>
     </div>
   )}
