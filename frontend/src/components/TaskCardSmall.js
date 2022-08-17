fa125f257c0a30cfe9f7040f42b16a011d4f8d2e
olof-sky <oloof92@gmail.com>
2022-08-17 12:31:39 +0200

    Add profile icon to small task card

--- frontend/src/components/TaskCardSmall.js
+++ frontend/src/components/TaskCardSmall.js
@@ -1,12 +1,22 @@
-import React,{useState} from 'react';
 import '../styles/components/TaskCardSmall.scss'
 
+const toggleShowProject = () => {
+  console.log("click")
+}
+
 function TaskCardSmall(props) {
+  let adminName = ""
+  let assigneeName = ""
+  try { adminName = props.task_admin_name.replace(' ', '+') } catch(err) {console.log(err)}
+  try { if (props.task_assignee) assigneeName = props.task_assignee.replace(' ', '+') } catch(err) {console.log(err)}
+  const adminIcon = `https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=${adminName}`
+  const assigneeIcon = `https://ui-avatars.com/api/?background=ffe253&color=000&name=${assigneeName}`
+
   return (
-    <div className="project-task-body">
+    <div onClick={ () => toggleShowProject() } className="project-task-body">
       <div className="project-task-card-small">
-        <p>{props.task_description}</p>
-        { props.task_asignee ? (<p>Assigned to: {props.task_asignee}</p>) : ( null ) }
+        <p>{ props.task_description }</p>
+        { props.task_assignee ? (<img className="profile-icon" id="icon" src={assigneeIcon} alt="SVG as an image"></img>) : (<img className="profile-icon" src={adminIcon} alt="SVG as an image"></img>) }
       </div>
     </div>
   )}
